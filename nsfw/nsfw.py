d83dcdda1d82c75d636c82420fbebbb1d6624efb
David Marby <david@dmarby.se>
2017-08-19 03:01:15 +0900

    Add remote debug support, convert tabs to spaces.

--- nsfw/nsfw.py
+++ nsfw/nsfw.py
@@ -4,6 +4,9 @@ from requests_file import FileAdapter
 from flask import Flask, request, jsonify
 from StringIO import StringIO
 
+if __name__ == '__main__' and os.environ.get('DEBUG', '0') is '1':
+    import ptvsd
+    ptvsd.enable_attach('docker-nsfw', address = ('0.0.0.0', 4002))
 
 s = requests.Session()
 s.mount('file://', FileAdapter())
@@ -13,12 +16,12 @@ app.config.from_object('config')
 
 # Caffe Stuff
 network = caffe.Net(
-	app.config['CAFFE_MODEL_DEFINITIONS'],
-	app.config['CAFFE_PRETRAINING'],
-	caffe.TEST
+    app.config['CAFFE_MODEL_DEFINITIONS'],
+    app.config['CAFFE_PRETRAINING'],
+    caffe.TEST
 )
 transformer = caffe.io.Transformer({
-	'data': network.blobs['data'].data.shape
+    'data': network.blobs['data'].data.shape
 })
 transformer.set_transpose('data', (2, 0, 1))
 transformer.set_mean('data', numpy.array([104, 117, 123]))
@@ -27,82 +30,83 @@ transformer.set_channel_swap('data', (2, 1, 0))
 
 
 def resize_image(image):
-	'''
-	Resizes the image to a size that is relevent to the yahoo data
-	'''
+    '''
+    Resizes the image to a size that is relevent to the yahoo data
+    '''
 
-	# If the image isn't in RGB mode, we will need to convert it.
-	if image.mode != 'RGB':
-		image = image.convert('RGB')
+    # If the image isn't in RGB mode, we will need to convert it.
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
 
-	# Now lets resize the image and save it into a StringIO buffer
-	# as a JPEG image.
-	rimg = image.resize((256, 256), resample=Image.BILINEAR)
-	resp = StringIO()
-	rimg.save(resp, format='JPEG')
+    # Now lets resize the image and save it into a StringIO buffer
+    # as a JPEG image.
+    rimg = image.resize((256, 256), resample=Image.BILINEAR)
+    resp = StringIO()
+    rimg.save(resp, format='JPEG')
 
-	# Return the byte array of the image.
-	resp.seek(0)
-	return bytearray(resp.read())
+    # Return the byte array of the image.
+    resp.seek(0)
+    return bytearray(resp.read())
 
 
 def compute(image_data):
-	'''
-	Computes the image nsfw score
-	'''
-	layers = app.config['CAFFE_OUTPUT_LAYERS']
+    '''
+    Computes the image nsfw score
+    '''
 
-	# Lets resize the image and load the image into Caffe
-	rimg = resize_image(image_data)
-	img = caffe.io.load_image(StringIO(rimg))
+    layers = app.config['CAFFE_OUTPUT_LAYERS']
 
-	# Compute the perceptional hash
-	phash = imagehash.average_hash(image_data)
+    # Lets resize the image and load the image into Caffe
+    rimg = resize_image(image_data)
+    img = caffe.io.load_image(StringIO(rimg))
 
-	# Now we will want to crop the image down.
-	H, W, _ = img.shape
-	_, _, h, w = network.blobs['data'].data.shape
-	h_off = max((H - h) /2, 0)
-	w_off = max((W - w) /2, 0)
-	crop = img[h_off:h_off + h, w_off:w_off + w, :]
+    # Compute the perceptional hash
+    phash = imagehash.average_hash(image_data)
 
-	# Now lets transform the image
-	timg = transformer.preprocess('data', crop)
-	timg.shape = (1,) + timg.shape
+    # Now we will want to crop the image down.
+    H, W, _ = img.shape
+    _, _, h, w = network.blobs['data'].data.shape
+    h_off = max((H - h) /2, 0)
+    w_off = max((W - w) /2, 0)
+    crop = img[h_off:h_off + h, w_off:w_off + w, :]
 
-	# And to get all of the outputs
-	outputs = network.forward_all(blobs=layers, **{network.inputs[0]: timg})
+    # Now lets transform the image
+    timg = transformer.preprocess('data', crop)
+    timg.shape = (1,) + timg.shape
 
-	# now to finally return the score
-	return {
-		'score': outputs[layers[0]][0].astype(float)[1] * 100,
-		'phash': str(phash)
-	}
+    # And to get all of the outputs
+    outputs = network.forward_all(blobs=layers, **{network.inputs[0]: timg})
+
+    # now to finally return the score
+    return {
+        'score': outputs[layers[0]][0].astype(float)[1] * 100,
+        'phash': str(phash)
+    }
 
 
 @app.route('/score', methods=['POST'])
 def get_score():
-	image = None
+    image = None
 
-	if 'image' in request.files:
-		image = Image.open(request.files['image'])
-	elif 'path' in request.form:
-		resp = s.get(request.form.get('path'), stream=True)
-		if resp.status_code == 200:
-			resp.raw.decode_content = True
-			image = Image.open(resp.raw)
+    if 'image' in request.files:
+        image = Image.open(request.files['image'])
+    elif 'path' in request.form:
+        resp = s.get(request.form.get('path'), stream=True)
+        if resp.status_code == 200:
+            resp.raw.decode_content = True
+            image = Image.open(resp.raw)
 
-	if image:
-		try:
-			scores = compute(image)
-		except:
-			return jsonify({'error': True})
-		else:
-			scores['error'] = False
-			return jsonify(scores)
-	else:
-		return jsonify({'error': True})
+    if image:
+        try:
+            scores = compute(image)
+        except:
+            return jsonify({'error': True})
+        else:
+            scores['error'] = False
+            return jsonify(scores)
+    else:
+        return jsonify({'error': True})
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
